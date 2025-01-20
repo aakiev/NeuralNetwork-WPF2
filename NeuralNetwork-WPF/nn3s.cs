3d2c5f8d7b696d29fb5ff99e099318d79130508e
Abdul-Malik Akiev <abdul-malik.akiev@smail.th-koeln.de>
2025-01-20 13:17:22 +0100

    Weights can be switched in the code, targets set to 0.9

--- NeuralNetwork-WPF/nn3s.cs
+++ NeuralNetwork-WPF/nn3s.cs
@@ -36,6 +36,30 @@ namespace NeuralNetwork_WPF
             wih = new double[inodes, hnodes];
             who = new double[hnodes, onodes];
 
+
+            wih[0, 0] = 0.9;    //Gewicht von Neuron 1 vom Inputlayer zu Neuron 1 vom Hiddenlayer
+            wih[1, 0] = 0.3;    //Gewicht von Neuron 2 vom Inputlayer zu Neuron 1 vom Hiddenlayer
+            wih[2, 0] = 0.4;    //Gewicht von Neuron 3 vom Inputlayer zu Neuron 1 vom Hiddenlayer
+            wih[0, 1] = 0.2;
+            wih[1, 1] = 0.8;
+            wih[2, 1] = 0.2;
+            wih[0, 2] = 0.1;
+            wih[1, 2] = 0.5;
+            wih[2, 2] = 0.6;
+            who[0, 0] = 0.3;
+            who[1, 0] = 0.7;
+            who[2, 0] = 0.5;
+            who[0, 1] = 0.6;
+            who[1, 1] = 0.5;
+            who[2, 1] = 0.2;
+            who[0, 2] = 0.8;
+            who[1, 2] = 0.1;
+            who[2, 2] = 0.9;
+
+
+
+            /*
+
             // Eine einzige Instanz von Random erzeugen
             Random random = new Random();
 
@@ -55,7 +79,7 @@ namespace NeuralNetwork_WPF
                 {
                     who[i, j] = random.NextDouble() * 1.0 - 0.5; // Werte im Bereich [-0.5, 0.5]
                 }
-            }
+            }*/
         }
 
 
